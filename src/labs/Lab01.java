025127d98fdfed46cb048233404b3d2a0e892210
akhillingutla <alingutla@newtech.coppellisd.com>
2022-03-21 14:28:40 -0500

    g

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -17,7 +17,15 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
-		
+		Scanner x = new Scanner(System.in);
+		System.out.println("Type 2 positive integers!");
+
+		int num = x.nextInt();
+		int num2 = x.nextInt();
+
+		double power = Math.pow(num, num2);
+
+		System.out.println(power);
 		
 	}
 	
@@ -26,7 +34,13 @@ public class Lab01 {
 	
 	public static void problem02() {
 		
-		
+		Scanner x = new Scanner(System.in);
+		System.out.println("Enter a positive number:");
+		int num = x.nextInt();
+
+		double y = Math.sqrt(num);
+		System.out.println("The square root of " + num + " is " + y);
+
 		
 	}
 
@@ -35,7 +49,21 @@ public class Lab01 {
 
 	public static void problem03() {
 		
-		
+		Scanner x = new Scanner(System.in);
+		System.out.println("Type the sides of a triangle");
+		int A = x.nextInt();
+		int B = x.nextInt();
+
+		int num = A*A;
+		int num2 = B*B;
+
+		int sum = num + num2;
+
+		double y = Math.sqrt(sum);
+
+		System.out.println("Hypotenuse = " + y);
+
+
 		
 
 	}
@@ -45,7 +73,23 @@ public class Lab01 {
 	
 	public static void problem04() {
 		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("enter an integer: ");
+
+		int x = inKey.nextInt();
+		int max = x;
+		int min = x;
+
+		while (x != 0) {
+
+		System.out.print("Enter an integer: ");
+		x = inKey.nextInt();
+		max = Math.max(max, x);
+		min = Math.min(min, x);
+		}
+
+
+		System.out.println("Max = " + max  + " || Min = "+  min);
 		
 	}
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -17,7 +17,25 @@ public class Lab02 {
 	
 	public static void problem01() {
 		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter an integer");
+
+		int x = inKey.nextInt();
+
+		System.out.println("Enter another integer");
+
+		int y = inKey.nextInt();
+
+
+		int max = Math.max(x, y);
+		int min = Math.min(x, y);
+
+		for (int i = 10; i > 0;) {
+
+		int total = (int)(Math.random()* (max - min+1)+ min);
+		System.out.println(total);
+		i--;
+		}
 		
 		
 	}
@@ -27,7 +45,16 @@ public class Lab02 {
 	
 	public static void problem02() {
 		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter the radi(s)us of the cylinder");
+		int radisus= inKey.nextInt();
+		System.out.println("Enter the height of the cylinder");
+		int x = inKey.nextInt();
+
+		double y = Math.PI * radisus * radisus);
+		double z = y * x;
+
+		System.out.println("The Volume of the Cylinder is: " + z);
 		
 	}
 
@@ -36,7 +63,19 @@ public class Lab02 {
 
 	public static void problem03() {
 
-		
+		Scanner inKey = new Scanner(System.in);
+		double num1 = Math.sqrt(10);
+		System.out.println("Enter the first x value");
+		int x1 = inKey.nextInt();
+		System.out.println("Enter the second x value");
+		int x2 = inKey.nextInt();
+		System.out.println("Enter the first y value");
+		int y1 = inKey.nextInt();
+		System.out.println("Enter the second y value");
+		int y2 = inKey.nextInt();
+		double total = ((x2-x1)-(x2 - x1))+((y2-y1)-(y2 - y1));
+		double dist = Math.sqrt(total);
+		System.out.println("Distance between points = " + dist);
 		
 	}
 
@@ -45,7 +84,21 @@ public class Lab02 {
 	
 	public static void problem04() {
 		
-		
+		double num1 = Math.sqrt(10);
+			Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter a-value");
+			int a = inKey.nextInt();
+		System.out.println("Enter b-value");
+			int b = inKey.nextInt();
+		System.out.println("Enter c-value");
+			int c = inKey.nextInt();
+		double y2 = ((b * b) - (4 *a * c));
+				double total1 = (-b + Math.sqrt(y2));
+		double total2 = (-b - Math.sqrt(y2));
+					double x1 = total1 / (2*a);
+		double x2 = total2 / (2*a);
+		System.out.println("x1 = " + x1);
+				System.out.println("x2 = " + x2);
 		
 		
 	}
